d7cf4e7b4835ba5377b656942b5e68ccb678208e
Ottavio Buonomo <ottavio.buonomo@live.com>
2025-06-06 14:09:04 +0200

    Add tests

---
+++ RecipeBookTests/RecipeBookTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeBookTests.swift
+//  RecipeBookTests
+//
+//  Created by Ottavio Buonomo on 06.06.2025.
+//
+
+import Testing
+
+struct RecipeBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

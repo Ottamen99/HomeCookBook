c206f2bf9c2bfac6090bddddfeb1ac4f19235a7d
Ottavio Buonomo <ottavio.buonomo@live.com>
2025-06-06 14:28:57 +0200

    Added tests

--- RecipeBookTests/RecipeBookTests.swift
+++ RecipeBookTests/RecipeBookTests.swift
@@ -1,16 +1,29 @@
-//
-//  RecipeBookTests.swift
-//  RecipeBookTests
-//
-//  Created by Ottavio Buonomo on 06.06.2025.
-//
+import XCTest
+@testable import RecipeBook
+import CoreData
 
-import Testing
+class RecipeBookTests: XCTestCase {
+    var mockContext: NSManagedObjectContext!
 
-struct RecipeBookTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        mockContext = TestCoreDataStack.managedObjectContext
     }
 
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockContext = nil
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(2 + 2, 4)
+    }
+
+    func testCoreDataStackInitialization() throws {
+        XCTAssertNotNil(mockContext, "The mock NSManagedObjectContext should not be nil after setUp.")
+    }
 }
